cbce31fa0d2e71c2136718f93a87bcee2f70d849
Aadit <aadits2004@gmail.com>
2026-06-11 13:31:28 +0530

    func for loading hf gpt2 model pretrained weights

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 import torch 
-from typing import cast
+from typing import cast, Self
 import math
 from torch import Tensor
 import torch.nn as nn
@@ -9,11 +9,11 @@ from torch.nn import functional as F
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embed: int = 384
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # no. of tokens
+    n_layer: int = 12
+    n_head: int = 12
+    n_embed: int = 768
     
 
 class CasualSelfAttention(nn.Module):
@@ -93,3 +93,50 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embed),
         ))
         self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+
+    
+    @classmethod
+    def from_pretrained(cls, model_type: str) -> "GPT":
+        
+        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+        
+        from transformers import GPT2LMHeadModel
+        print(f"Loading weights from pretrained: {model_type}")
+
+        config_args = {
+            "gpt2": dict(n_layer=12, n_head=12, n_embed=768),
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embed=1024),
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embed=1280),
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embed=1600)
+        }[model_type]
+
+        config_args['vocab_size'] = 50527
+        config_args['block_size'] = 1024
+
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith(".attn.bias")]
+
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.masked.bias")]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.bias")] 
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        assert len(sd_keys_hf) == len(sd_keys), f"Mismatched keys {sd_keys_hf} != {sd_keys}"
+
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
