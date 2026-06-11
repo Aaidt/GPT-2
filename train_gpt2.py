5397c9458f6354066aa51f21b58563fdbe62d5e7
Aadit <aadits2004@gmail.com>
2026-06-11 09:54:17 +0530

    mlp fon

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import torch 
+from torch import Tensor
 import torch.nn as nn
 from torch.nn import functional as F
 
@@ -12,9 +13,61 @@ class GPTConfig:
     n_head: int = 6
     n_embed: int = 384
     
+
+class CasualSelfAttention(nn.Module):
+    
+    def __init__(self, config: GPTConfig) -> None:
+        super().__init__()
+        assert config.n_embed % config.n_head == 0
+        self.c_attn = nn.Linear(config.n_embed, 3 * config.n_embed)
+        self.c_proj = nn.Linear(config.n_embed, config.n_embed)
+        self.n_embed = config.n_embed
+        self.n_head = config.n_head
+
+        # self.register_buffer("bias", )
+
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config: GPTConfig) -> None:
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embed, config.n_embed * 4)
+        self.gelu = nn.GELU(approximate="tanh")
+        self.c_proj = nn.Linear(4 * config.n_embed, config.n_embed)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class Block(nn.Module):
+    
+    def __init__(self, config: GPTConfig) -> None:
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embed)
+        self.attn = CasualSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embed)
+        self.mlp = MLP(config)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
 class GPT(nn.Module):
 
-    def __init__(self, config):
+    def __init__(self, config: GPTConfig) -> None:
         super().__init__()
         self.config = config
         
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embed),
+            wpe = nn.Embedding(config.block_size, config.n_embed),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_embed),
+        ))
+        self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
